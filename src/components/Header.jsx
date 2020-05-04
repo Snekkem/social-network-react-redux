34f6eafadd8d883611a5bdbcf527c055117356a4
Snekkem <49142008+Snekkem@users.noreply.github.com>
2020-05-04 21:15:29 +0300

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IMessage_logo.svg/600px-IMessage_logo.svg.png"></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+            </div>
+            <div>
+                ava + decr
+            </div>
+
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>)
+}
+
+export default Profile;
